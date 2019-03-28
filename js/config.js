9e0cc80dafa125321e9bfcbbe847186cbb270bbb
fengFly_Zhang <i@zhf6.com>
2019-03-28 21:15:04 +0800

    iocc

---
+++ js/config.js
@@ -0,0 +1,22 @@
+// array of Monitor-specific API keys or Main API key to list all monitors
+var __apiKeys = [
+	// 'm776858319-7c829d2c3e250f85776f588e', //Community
+	// 'm776858325-1ddf7c7e98ce56609325db3b', //Document
+	// 'm776858327-19b31e671f6b217b8ded96c6', //Me
+	// 'm776858407-a704cd548fb5c15db5b625f9', //Update A
+	// 'm778126978-24e6f1ce275c2518bad1e925', //Update B
+	// 'm777157148-a67f71b8a807c54b8ce92c9d', //Update deliver A
+	// 'm777157137-ce781489d1af15253ca3f0c1', //Update deliver B
+	// 'm776858320-7b44f9ac136f69ac3e356e8e', //Auth A
+	// 'm776858321-486827042ed7a7f7b8952a05', //Auth B
+	// 'm778734196-a51947f061c5db10cee048f7', //Auth C
+	// 'm776858323-0bdafb9e9a71e3305c27c1af', //Auth D
+	// 'm777190463-ac0ab733e1b29c92b7de0354', //Auth E
+    'm782292523-90cb14848da893f4cd61f02e', //myblog
+    'm782292509-ae5f6be6e2087e721594700e', //aufer
+    'm782292540-b31a4da883c8ba4758e8dd5e', //iocc.dev
+    
+];
+
+// refresh interval (in seconds)
+var __refresh = 180;
